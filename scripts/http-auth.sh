e1bcc0ca61a6299e821b690b603a8bda366a4ac2
Jens Krause <jens@crazy-factory.com>
2017-07-13 15:47:49 +0700

    validated json files

--- scripts/http-auth.sh
+++ scripts/http-auth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-printf "{'repositories': { 'local':{ 'type': 'composer', 'url': 'https://php.fury.io/crazyfactory/'} } }" > ~/.composer/config.json
+printf '{"repositories": { "local":{ "type": "composer", "url": "https://php.fury.io/crazyfactory/"} } }' > ~/.composer/config.json
 composer global config -l | grep http
-printf "{'http-basic': {'php.fury.io': { 'username': '$GEMFURY_TOKEN', 'password': ''}}" > ~/auth.json
+printf '{"http-basic": {"php.fury.io": { "username": "$GEMFURY_TOKEN", "password": ""}}}' > ~/auth.json
