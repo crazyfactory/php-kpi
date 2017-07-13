55b1c37312ac6b16f711c96bef7baa17dabde78c
Jens Krause <jens@crazy-factory.com>
2017-07-13 15:36:48 +0700

    adjust global config.json for composer, add debug info

--- scripts/http-auth.sh
+++ scripts/http-auth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-printf "{'repositories': [{ 'type': 'composer', 'url': 'https://php.fury.io/crazyfactory/'}]}" > ~/config.json
+printf "{'repositories': { 'local':{ 'type': 'composer', 'url': 'https://php.fury.io/crazyfactory/'} } }" > ~/.composer/config.json
+composer global config -l | grep http
 printf "{'http-basic': {'php.fury.io': { 'username': '$GEMFURY_TOKEN', 'password': ''}}" > ~/auth.json
