aad8b44b030288ef66835b2adb4154c6058d95de
Jens Krause <jens@crazy-factory.com>
2017-07-14 07:26:43 +0700

    feat(http-auth) dynamic generation of ~/.composer/config.json  with token

--- scripts/http-auth.sh
+++ scripts/http-auth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf '{"repositories": [ { "type": "composer", "url": "https://php.fury.io/$GEMFURY_TOKEN/crazyfactory/"} ] }' > ~/.composer/config.json
+printf "{\"repositories\": [ { \"type\": \"composer\", \"url\": \"https://php.fury.io/$GEMFURY_TOKEN/crazyfactory/\"} ] }" > ~/.composer/config.json
 composer global config -l | grep http
