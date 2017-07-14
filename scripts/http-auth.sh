1052bdf700d3d98475e167ad50afbc77c6e06c92
Jens Krause <jens@crazy-factory.com>
2017-07-14 07:59:19 +0700

    use auth.json to hold composer credentials

--- scripts/http-auth.sh
+++ scripts/http-auth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-printf "{\"repositories\": [ { \"type\": \"composer\", \"url\": \"https://php.fury.io/$GEMFURY_TOKEN/crazyfactory/\"} ] }" > ~/.composer/config.json
+printf "{\"repositories\": [ { \"type\": \"composer\", \"url\": \"https://php.fury.io/crazyfactory/\"} ] }" > ~/.composer/config.json
+printf "{ \"http-basic\": {\"php.fury.io\": { \"username\": \"$GEMFURY_TOKEN\", \"password\": \"\"}}" > ~/.composer/auth.json
 composer global config -l | grep http
