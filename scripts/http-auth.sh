29992e3f2853e8f7c036d4191c4641fd14f82709
Jens Krause <jens@crazy-factory.com>
2017-07-14 08:05:17 +0700

    add missing } to auth.json

--- scripts/http-auth.sh
+++ scripts/http-auth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 printf "{\"repositories\": [ { \"type\": \"composer\", \"url\": \"https://php.fury.io/crazyfactory/\"} ] }" > ~/.composer/config.json
-printf "{ \"http-basic\": {\"php.fury.io\": { \"username\": \"$GEMFURY_TOKEN\", \"password\": \"\"}}" > ~/.composer/auth.json
+printf "{ \"http-basic\": {\"php.fury.io\": { \"username\": \"$GEMFURY_TOKEN\", \"password\": \"\"}]}" > ~/.composer/auth.json
 composer global config -l | grep http
